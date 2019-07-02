914a621066b86d696c559eca17cc7da6dd82e2cc
Daniel Laube <mail@dlaube.de>
2019-07-03 00:20:06 +0200

    Update simple-arch.sh

--- simple-arch.sh
+++ simple-arch.sh
@@ -32,7 +32,7 @@ arch-chroot /mnt /bin/bash -c "echo \"127.0.0.1        localhost\"  > /etc/hosts
 arch-chroot /mnt /bin/bash -c "echo \"::1              localhost\" >> /etc/hosts"
 arch-chroot /mnt /bin/bash -c "echo \"127.0.1.1        $SIMPLE_HOSTNAME\" >> /etc/hosts"
 
-arch-chroot /mnt /bin/bash -c "echo \"Server = https://mirror.orbit-os.com/archlinux/\$repo/os/\$arch\" > /etc/pacman.d/mirrorlist"
+arch-chroot /mnt /bin/bash -c "echo \"Server = https://mirror.orbit-os.com/archlinux/\\$repo/os/\\$arch\" > /etc/pacman.d/mirrorlist"
 
 arch-chroot /mnt /bin/bash -c "pacman --noconfirm -Sy grub"
 arch-chroot /mnt /bin/bash -c "grub-install $SIMPLE_DISK"
